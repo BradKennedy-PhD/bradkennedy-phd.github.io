3f95de0b1fc0081c22f551b419216b8b2e567a01
Bradley Kennedy <b.kennedy@chester.ac.uk>
2021-11-22 13:57:01 +0000

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,7 +1,19 @@
 // details here https://sweetalert2.github.io/
 
+let timerInterval
 Swal.fire({
   icon: 'info',
   title: 'Working on it...',
-  text: 'These web pages are still being created. Some things might not work yet.'
+  text: 'These web pages are still being created. Some things might not work yet.',
+  timer: 5000,
+  timerProgressBar: true,
+  willClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
 })
+
